da862056a0b981946c270e8b04ee4b3ce06fede9
J <jb.bgc@hotmail.com>
2020-12-08 14:45:31 -0600

    broken homepage into file structures

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './homepage.styles.scss';
+import Directory from '../../components/directory/directory.component'
+
+const HomePage = () => (
+    <section>
+    <div className='homepage'>
+        <Directory/>
+    </div>
+</section>        
+);
+
+export default HomePage
