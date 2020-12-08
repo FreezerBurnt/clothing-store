a6c66d31b741f1a1cf374712c5bc950a6993d4c8
J <jb.bgc@hotmail.com>
2020-12-08 16:23:16 -0600

    updated menu items nav wRouter

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -5,7 +5,7 @@ import Directory from '../../components/directory/directory.component'
 const HomePage = () => (
     <section>
     <div className='homepage'>
-        <Directory/>
+        <Directory />
     </div>
 </section>        
 );
